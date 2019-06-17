683a8a9bdbd89c12da5d57f36a15e282040bf28b
chaitanya-lakkundi <chaitanya.lakkundi@gmail.com>
2019-06-17 16:46:29 +0530

    remove seaborn dependency

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:18.04
 
 RUN apt-get update && \
-        apt-get install -y python3 python3-setuptools git-core curl doxygen \
+        apt-get install -y python3 python3-pip git-core curl doxygen \
         graphviz qpdf
 
 WORKDIR "/tmp"
@@ -9,9 +9,7 @@ WORKDIR "/tmp"
 RUN curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb && \
         dpkg -i ripgrep_11.0.1_amd64.deb
 
-RUN git clone https://github.com/mwaskom/seaborn.git && \
-        cd seaborn && \
-        python3 setup.py install
+RUN pip3 install matplotlib
 
 WORKDIR "/home"
 
--- install.sh
+++ install.sh
@@ -3,24 +3,21 @@
 CURDIR="$PWD"
 
 echo -e "\nWelcome to CodeBus install\n"
-echo -e "\nWe will be installing dependencies: python3, python3-setuptools\ngit-core, curl, doxygen, graphviz, qpdf, ripgrep and seaborn"
+echo -e "\nWe will be installing dependencies: python3, python3-pip\ngit-core, curl, doxygen, graphviz, qpdf and ripgrep"
 echo -e "\n\nPress enter to proceed\n"
 read
 
 sudo apt-get update
 
-sudo apt-get install python3 python3-setuptools git-core curl doxygen graphviz qpdf
+sudo apt-get install python3 python3-pip git-core curl doxygen graphviz qpdf
 
 cd /tmp
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb
 sudo dpkg -i ripgrep_11.0.1_amd64.deb
 
-git clone https://github.com/mwaskom/seaborn.git
-cd seaborn
+echo -e "\nInstalling Matplotlib..\n"
 
-echo -e "\nInstalling seaborn globally\n\nPress enter to proceed"
-read
-sudo python3 setup.py install
+sudo pip3 install matplotlib
 
 cd "$CURDIR"
 mkdir Repos
